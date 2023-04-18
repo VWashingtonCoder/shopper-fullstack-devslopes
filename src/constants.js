f9bfd64575c5d87f00eb9ae5d1acd1df82ebd0f2
V. Andre Washington <kuraiokamiblaze@gmail.com>
2023-04-18 13:39:50 -0600

    Update const file with commercejs headers

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,4 @@
 export const COMMERCE_API_URL = "https://api.chec.io/v1/products";
-export const COMMERCE_AUTH_KEY = process.env.REACT_APP_PUBLIC_AUTHORIZATION;
+export const COMMERCE_API_HEADERS = {
+    "x-Authorization": process.env.REACT_APP_PUBLIC_AUTHORIZATION
+}
