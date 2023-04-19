59162880275003979b6977d51c1c935772b5f75e
V. Andre Washington <kuraiokamiblaze@gmail.com>
2023-04-19 10:40:33 -0600

    Set up init states

--- src/components/ShopperApp/ShopperApp.jsx
+++ src/components/ShopperApp/ShopperApp.jsx
@@ -5,15 +5,22 @@ const products = new ProductService();
 
 class ShopperApp extends React.Component {
     state = {
-        data: [],
-        
+        accounts: [
+            {id: 0, username: "admin", pw: "Password$1"}
+        ],
+        activeAccount: {},
+        cart: [],
+        page: "home",
+        payCard: {},
+        products: [],
+        shipAddress: {},
     }
 
     componentDidMount() {
         products.fetchProducts().then((res) => {
             if (res && res.resp.ok){
                 this.setState({
-                    data: res.data
+                    products: res.data
                 });
             }
         }).catch(err => {
