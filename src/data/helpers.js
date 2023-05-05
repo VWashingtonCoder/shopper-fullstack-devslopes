25f57d1c654ea549510a3aaf4d4af42a88cb5c7f
V. Andre Washington <kuraiokamiblaze@gmail.com>
2023-05-05 11:10:40 -0600

    Add logic tp toggle disabled submit on signup form

--- src/data/helpers.js
+++ src/data/helpers.js
@@ -13,3 +13,30 @@ export function generateStockQtys(stock) {
   });
   return stockQuantities;
 }
+
+export function validateSignUpDisabled(formArr) {
+  const validArr = [];
+  let name = "";
+  let value = "";
+  let valLength = 0;
+
+  for (let entry of formArr) {
+    name = entry[0];
+    value = entry[1];
+    valLength = value.length;
+
+    if (name === "firstName" || name === "surname") {
+      console.log(valLength);
+      valLength >= 2 ? validArr.push("valid") : validArr.push("invalid");
+    } else if (name === "email") {
+      valLength > 0 && value.includes("@") && value.includes(".")
+        ? validArr.push("valid")
+        : validArr.push("invalid");
+    } else if (name === "password" || name === "confirm") {
+      valLength >= 8 ? validArr.push("valid") : validArr.push("invalid");
+    }
+  }
+  
+  if (!validArr.includes("invalid")) return false;
+  else return true;
+}
